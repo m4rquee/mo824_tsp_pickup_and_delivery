93f7d421c5c291bf70a6edf119d93a9b94cf6b73
m4rquee <lucasxm12@gmail.com>
2022-07-05 16:15:43 -0300

    new lp metrics and out file format

--- instances/instance_gen.py
+++ instances/instance_gen.py
@@ -5,31 +5,33 @@ np.random.seed(42)
 square_dim = 1000
 tests = 3
 N = [5, 6, 7, 8, 9, 10, 15, 20, 25, 50, 75, 100, 150, 200, 300]
+P = [1, 2, np.inf]
 
 
-def dist(p, q):
-    return ceil(np.linalg.norm(p - q))
+def dist(p, q, ord): return ceil(np.linalg.norm(p - q, ord=ord))
 
 
-def print_to_file(n, label, points):
-    with open(f'l2{n}{label}.dig', 'w') as f_out:
+def print_to_file(n, points, ord):
+    with open(f'l{ord}-{n}.dig', 'w') as f_out:
         print('nnodes narcs type', file=f_out)
         v = len(points)
         print(f'{v} {v * (v - 1)} digraph', file=f_out)
 
         print('nodename posx posy', file=f_out)
-        for i, point in enumerate(points):
-            print(f'{i} {point[0]} {point[1]}', file=f_out)
+        print(f'{0} {points[0][0]} {points[0][1]}', file=f_out)  # source
+        print(f'{v - 1} {points[v - 1][0]} {points[v - 1][1]}', file=f_out)  # target
+        for i in range(1, n + 1):
+            print(f'{i} {points[i][0]} {points[i][1]}', file=f_out)
+            print(f'{i + n} {points[i + n][0]} {points[i + n][1]}', file=f_out)
 
         print('tail head weight', file=f_out)
         for i, tail in enumerate(points):
             for j, head in enumerate(points):
                 if i == j: continue
-                print(f'{i} {j} {dist(tail, head)}', file=f_out)
+                print(f'{i} {j} {dist(tail, head, ord)}', file=f_out)
 
 
-a_num = ord('a')
 for n in N:
-    for label in range(a_num, a_num + tests):
+    for p in P:
         points = np.random.randint(square_dim + 1, size=(2 * n + 2, 2))
-        print_to_file(n, chr(label), points)
+        print_to_file(n, points, p)
